81cc09c7f4cdf78ffdf8d750e14d06377b73ad3b
Hasan Rafi Ahmed <rafi@Hasans-Mac-mini.local>
2026-03-04 15:13:46 +0600

    feat(components): add reusable shared container with max-width 1440px

---
+++ src/app/(withCommonLayout)/page.tsx
@@ -0,0 +1,10 @@
+import Banner from '@/components/home/Banner'
+import React from 'react'
+
+export default function page() {
+  return (
+      <div>
+          <Banner></Banner>
+    </div>
+  )
+}
